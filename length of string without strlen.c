d0b2593daf9a66bcd817b1521512140a4e1720af
msritahmed <166241081+msritahmed@users.noreply.github.com>
2024-08-30 17:33:36 +0530

    Create length of string without strlen.c

---
+++ length of string without strlen.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<unistd.h>
+#include<math.h>
+void main()
+ {
+    int l=0;
+    char a[10];
+    printf("enter the string\n");
+    gets(a); //also use scanf("%s",a);
+             //fgets takes the '\n' takes for count
+     
+     for(int i=0;a[i]!=0;i++)
+     {
+         l++;
+     }
+     printf("lenght of string:%d",l);
+     
+    
+ }
